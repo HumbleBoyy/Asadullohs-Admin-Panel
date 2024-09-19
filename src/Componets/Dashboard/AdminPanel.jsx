c83ac06f231886198f83408c4564a92b9c72e53a
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-09-20 03:08:37 +0500

    Sign out function implemented succefully

--- src/Componets/Dashboard/AdminPanel.jsx
+++ src/Componets/Dashboard/AdminPanel.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { auth } from '../../firebase'
 
 const AdminPanel = () => {
   return (
     <>
-      Admin Panel
+      <div className='dasboard'>
+          <form>
+            <input type="text" placeholder='Project Name'/>
+            <textarea type="text" placeholder='Description'/>
+            <input type="text" placeholder='Project Domain Link'/>
+            <input type="text" placeholder='GitHub Reposotory Link'/>
+            <button onClick={() => auth.signOut()}>Sign Out</button>
+            <button type='submit'>Submit</button>
+          </form>
+      </div>
     </>
   )
 }
