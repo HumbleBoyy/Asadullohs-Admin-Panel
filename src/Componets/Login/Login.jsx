44f11e946172be1be380d44a9f36302066831f6b
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-09-21 01:02:39 +0500

    Just commit for this

--- src/Componets/Login/Login.jsx
+++ src/Componets/Login/Login.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { signInWithGoogle } from '../../firebase'
+import { loginWithGoogle } from '../../firebase'
 
 const Login = () => {
   return (
     <>
        <div className="login_btn_wrapper">
-          <button className='login_btn' onClick={signInWithGoogle}>LogIn with google</button>
+          <button className='login_btn' onClick={loginWithGoogle}>LogIn with google</button>
        </div>
     </>
   )
